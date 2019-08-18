4ab7f4402d46def597199dafbd7816a6d148fde7
Matthew Zelinsky <zelinsky@udel.edu>
2019-08-17 21:35:31 -0400

    Changes

--- events/message.js
+++ events/message.js
@@ -1,5 +1,5 @@
 module.exports = (client, message) => {
-  if(!message.content.startsWith(client.config.prefix) || message.author.bot) return
+  if(!message.channel.name != 'bot' ||!message.content.startsWith(client.config.prefix) || message.author.bot) return
   
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g)
   const command = args.shift().toLowerCase()
