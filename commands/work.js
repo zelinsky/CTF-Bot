272de12e1a9a264a8f3298599f24a5b7e5dda83f
Matthew Zelinsky <zelinsky@udel.edu>
2019-08-18 19:47:27 -0400

    Added roles for CTF problems.

---
+++ commands/work.js
@@ -0,0 +1,28 @@
+exports.run = (client, message, args) => {
+
+  if(!message.channel.parent || message.channel.name === 'general')
+    return message.delete().then(message => message.reply("You must use the **work** command in a CTF problem channel!")).catch(console.error)
+
+  const ctfname = message.channel.parent.name
+  const problem = message.channel.name
+  const newRole = message.guild.roles.find(role => role.name === `${ctfname}-${problem}`)
+
+  if (!newRole)
+    return console.log(`The ${ctfname}-${problem} role does not exist`)
+
+  const prevRole = message.member.roles.find(role => role.name.startsWith(ctfname))
+  if (prevRole)
+    message.member.removeRole(prevRole).catch(console.error)
+
+  if (prevRole !== newRole)
+    return message.delete().then(message => message.member.addRole(newRole)).catch(console.error)
+  else
+    return message.delete().catch(console.error)
+
+}
+
+exports.help = {
+  name: "work",
+  description: "Will assign you to be working on the CTF problem of the current channel. Will unassign you from CTF problem if already assigned. Must be used in a CTF Problem channel.",
+  usage: "work"
+};
--- events/message.js
+++ events/message.js
@@ -1,7 +1,7 @@
 module.exports = (client, message) => {
   if(!message.content.startsWith(client.config.prefix) || message.author.bot) return
 
-  if (message.channel.name !== 'bot' && !message.content.startsWith('!join')) return
+  if (message.channel.name !== 'bot' && !message.content.startsWith(`${client.config.prefix}join`) && !message.content.startsWith(`${client.config.prefix}work`)) return
 
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g)
   const command = args.shift().toLowerCase()
