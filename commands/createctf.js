5436d4a69a779c4553669fb7f3e42dfb247d1fe7
Matthew Zelinsky <zelinsky@udel.edu>
2019-08-19 17:52:54 -0400

    Made code prettier

--- commands/createctf.js
+++ commands/createctf.js
@@ -1,32 +1,46 @@
+// Creates a category named `ctf` and channels under that category for each problem in `problems`
+// Also creates roles for each problem
+
 exports.run = async (client, message, [ctf, ...problems]) => {
 
+    // Checks if at least one argument (the ctf name) was given
     if (!ctf)
-	return message.reply("You must specify a CTF to create.").catch(console.error)
+	return message.reply("You must specify a CTF to create.").catch(console.error);
 
-    const ctfname = ctf.toLowerCase()
+    const ctfname = ctf.toLowerCase();
 
-    let ctfcat = message.guild.channels.find(channel => (channel.name.toLowerCase() === ctfname) && channel.type === 'category')
+    // Finds ctf category if it already exists
+    let ctfcat = message.guild.channels.find(channel => (channel.name === ctfname) && channel.type === 'category');
+
+    let output = ''
     
+    // If ctf category doesn't exist, create it and add a general channel to it
     if (!ctfcat) {
 	ctfcat = await message.guild.createChannel(ctfname, {type:'category'}).then(category => {
-	    message.guild.createChannel('general', {type:'text', parent:category}).then(message.reply(`Created ${ctfname} category!`)).then(message.reply(`Created general channel for ${ctfname}!`))
-	    return category
-	}).catch(console.error)
+	    message.guild.createChannel('general', {type:'text', parent:category}).catch(console.error);
+	    return category;
+	}).catch(console.error);
+	output += `+ [category] ${ctfname}\n+ [channel]  general\n`;
     }
 
+    // If problem names are specified after ctf name, create a channel and role for each problem (if it  doesn't already exist) under the ctf category
+    // Roles created are equivalent to the Member role and are intended for use with the !work command
     if (problems.length !== 0) {
-	problems = problems.map(p => p.toLowerCase())
+	problems = problems.map(p => p.toLowerCase());
 
 	problems.forEach(problem => {
 	    if (!ctfcat.children.find(channel => channel.name === problem))
-		message.guild.createChannel(problem, {type:'text', parent:ctfcat}).then(channel => message.guild.createRole({name:`${ctfname}-${channel.name}`, color:'BLUE', hoist:true, permissions: 104324673})).then(message.reply(`Created ${problem} channel for ${ctfname}!`)).catch(console.error)
-	})
+		message.guild.createChannel(problem, {type:'text', parent:ctfcat}).then(channel => message.guild.createRole({name:`${ctfname}-${channel.name}`, color:'BLUE', hoist:true, permissions: 104324673})).then(output += `+ [channel]  ${problem}\n`).catch(console.error);
+	});
     }
+
+    // Send output
+    message.channel.send(output, {code: 'diff'});
 };
 
 exports.conf = {
     permLevel: "Mod"
-}
+};
 
 exports.help = {
     name: "createctf",
--- commands/deletectf.js
+++ commands/deletectf.js
@@ -1,27 +1,37 @@
+// Deletes the ctf category names `ctf` and all children channels and corresponding roles
+
 exports.run = async (client, message, [ctf, ...problems]) => {
 
+    // Checks if at least one argument (the ctf name) was given
     if (!ctf)
-	return message.reply("You must specify a CTF to create.").catch(console.error)
+	return message.reply("You must specify a CTF to create.").catch(console.error);
 
-    const ctfname = ctf.toLowerCase()
-    const ctfcat = message.guild.channels.find(channel => (channel.name.toLowerCase() === ctfname) && channel.type === 'category')
+    const ctfname = ctf.toLowerCase();
 
+    // Finds ctf category
+    const ctfcat = message.guild.channels.find(channel => (channel.name.toLowerCase() === ctfname) && channel.type === 'category');
+
+    // Return with error message if not found
     if (!ctfcat)
-	return message.reply(`${ctfname} not found!`).catch(console.error)
+	return message.reply(`${ctfname} not found!`).catch(console.error);
+
+    let output = `- [category] ${ctfname}\n`;
     
+    // For each channel of the category, delete the channel and corresponding role
     ctfcat.children.forEach(channel => {
 	if (channel.name !== 'general')
-	    message.guild.roles.find(role => role.name === `${ctfname}-${channel.name}`).delete().catch(console.error)
-	channel.delete().then(message.reply(`Deleted ${channel.name} channel of ${ctfname}!`)).catch(console.error)
-    })
+	    message.guild.roles.find(role => role.name === `${ctfname}-${channel.name}`).delete().catch(console.error);
+	channel.delete().then(output += `- [channel]  ${channel.name}\n`).catch(console.error);
+    });
 
-    ctfcat.delete().then(message.reply(`Deleted ${ctfname} category!`)).catch(console.error)
-    
+    // Delete the category and send output
+    ctfcat.delete().catch(console.error);
+    message.channel.send(output, {code: 'diff'});
 };
 
 exports.conf = {
     permLevel: "Mod"
-}
+};
 
 exports.help = {
     name: "deletectf",
