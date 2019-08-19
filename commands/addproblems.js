400fb28c3eeb5765512ea27a65ac7789209c186e
Matthew Zelinsky <zelinsky@udel.edu>
2019-08-18 23:17:07 -0400

    Added more features.

---
+++ commands/addproblems.js
@@ -0,0 +1,27 @@
+exports.run = (client, message, [ctf, ...problems]) => {
+
+  if (!ctf)
+    return message.reply("You must specify a CTF to add problems to.").catch(console.error)
+
+  const ctfname = ctf.toLowerCase()
+
+  const ctfchannel = message.guild.channels.find(channel => (channel.name.toLowerCase() === ctfname) && channel.type === 'category')
+  if (!ctfchannel)
+    return message.reply(`${ctfname} not found!`).catch(console.error)
+
+  if (problems.length === 0)
+    return message.reply("You must specify at least one problem to add.").catch(console.error)
+
+  problems = problems.map(p => p.toLowerCase())
+
+  problems.forEach(problem => {
+    if (!ctfchannel.children.find(channel => channel.name === problem))
+      message.guild.createChannel(problem, {type:'text', parent:ctfchannel}).then(channel => message.guild.createRole({name:`${ctfname}-${channel.name}`, color:'BLUE', hoist:true, permissions: 104324673})).then(message.reply(`Created ${problem} channel for ${ctfname}!`)).catch(console.error)
+  })
+}
+
+exports.help = {
+  name: "addproblems",
+  description: "Creates channels for problems for the specified CTF category.",
+  usage: "addproblems ctf problem [...problems]"
+};
---
+++ commands/createctf.js
@@ -0,0 +1,25 @@
+exports.run = (client, message, [ctf, ...problems]) => {
+
+  if (!ctf)
+    return message.reply("You must specify a CTF to create.").catch(console.error)
+
+  const ctfname = ctf.toLowerCase()
+  problems = problems.map(p => p.toLowerCase())
+
+  if (message.guild.channels.find(channel => (channel.name.toLowerCase() === ctfname) && channel.type === 'category'))
+    return message.reply(`${ctfname} already exists!`).catch(console.error)
+
+  message.guild.createChannel(ctfname, {type:'category'}).then(category => {
+    message.guild.createChannel('general', {type:'text', parent:category}).then(message.reply(`Created general channel for ${ctfname}!`)).catch(console.error)
+
+    problems.forEach(problem => {
+      message.guild.createChannel(problem, {type:'text', parent:category}).then(channel => message.guild.createRole({name:`${ctfname}-${channel.name}`, color:'BLUE', hoist:true, permissions: 104324673})).then(message.reply(`Created ${problem} channel for ${ctfname}!`)).catch(console.error)
+    })
+  }).then(message.reply(`Created ${ctfname} category!`)).catch(console.error)
+}
+
+exports.help = {
+  name: "createctf",
+  description: "Creates a category for the CTF including a general channel and channels for any specified problems.",
+  usage: "createctf ctf [...problems]"
+};
