293e757c92214cd9da0e0c4a6420d9f6a483d6df
Matthew Zelinsky <zelinsky@udel.edu>
2019-09-19 11:54:52 -0400

    Updated config.

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
     token: process.env.BOT_TOKEN, // Envrionment variable BOT_TOKEN = bot_token_here
+    member_flag: process.env.MEMBER_FLAG, // Member flag for joining server
     prefix: '!', // Command prefix
     perms: ['@everyone', 'Member', 'Mod', 'Admin'], // Index corresponds to rank level
     timeout: 3000, // Time to wait before deleting messages
