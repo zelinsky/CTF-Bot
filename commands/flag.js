d0afea6542181d03215d431f23b45dfdef6f2005
Matthew Zelinsky <zelinsky@udel.edu>
2019-09-19 11:49:12 -0400

    Updated the flag command.

--- commands/flag.js
+++ commands/flag.js
@@ -9,13 +9,32 @@ exports.run = async (client, message, flags) => {
     if (flags.length === 0)
 	return message.reply("You must specify a flag.").catch(console.error);
 
-    // For each flag, send message in #flags
+    // Send message in #flags
     const flagChannel = message.guild.channels.find(channel => channel.name === 'flags' && !channel.parent);
     
     // Remove emoji prefix if present
     let problem = message.channel.name.startsWith(client.config.flag) ? message.channel.name.substr(client.config.flag.length) : message.channel.name;
-    let f = flags.join('');
-    flagChannel.send(`__**${message.channel.parent.name.toUpperCase()}**__ - **${message.member.displayName}** captured the flag \`${f}\` for **${problem}**!`);
+    let f = "";
+
+    // Check for -d option at beginning or end
+    if (flags[0] === '-d') {
+	f = flags.slice(1).join(' ');
+    } else if (flags[flags.length-1] === '-d') {
+	f = flags.slice(0, -1).join(' ');
+    } else { // No -d option
+	f = flags.join(' ');
+	
+	// If channel is unmarked, mark it
+	if (!message.channel.name.startsWith(client.config.flag)) 
+	    message.channel.setName(`${client.config.flag}${message.channel.name}`).catch(console.error);
+    }
+
+    // i.e. if the command was not '!flag -d'
+    if (f)
+	flagChannel.send(`__**${message.channel.parent.name.toUpperCase()}**__ - **${message.member.displayName}** captured the flag \`${f}\` for **${problem}**!`);
+    else
+	return message.reply("You must specify a flag.").catch(console.error);
+
 };
 
 exports.conf = {
@@ -27,6 +46,6 @@ exports.conf = {
 exports.help = {
     name: "flag",
     category: "CTF Problem",
-    description: "Declares you found a flag for the problem of the current channel.",
-    usage: "flag ctf{fl4g}"
+    description: "Declares you found a flag for the problem of the current channel, and marks the channel with a flag emoji. If you don't want to mark the channel, use the -d flag.",
+    usage: "flag [-d] ctf{fl4g}"
 };
