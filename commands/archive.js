abbf915b8ef9936822e289bd229a6344c2da516a
Micah Pope <micah@micahpope.com>
2020-05-23 09:45:59 -0700

    Update archive to iterate over a whole category

--- commands/archive.js
+++ commands/archive.js
@@ -1,27 +1,56 @@
-exports.run = async (client, message, args) => {
-  const all_messages_array = lots_of_messages_getter(message.channel);
+exports.run = async (client, message, [ctf]) => {
 
+  // Checks if at least one argument (the ctf name) was given
+  if (!ctf) return message.reply("You must specify a CTF to archive.").catch(console.error);
+
+  const ctfname = ctf.toLowerCase();
+
+  // Finds ctf category
+  const ctfcat = message.guild.channels.find(channel => (channel.name.toLowerCase() === ctfname) && channel.type === 'category');
+
+  // Return with error message if not found
+  if (!ctfcat)
+  return message.reply(`${ctfname} not found!`).catch(console.error);
+
+  var working_obj = {};
   var final_obj = {};
+  var test_arr = [];
 
-  all_messages_array.then(messages =>{ 
-    for (const msg of messages.reverse()) {
-      // console.log(`${msg.author.username} : ${msg.id}`);
-      final_obj[msg.id] = {};
-      final_obj[msg.id].author = msg.author.username;
-      final_obj[msg.id].content = msg.content;
-    }
-    console.log(final_obj);
+  var file_path = ctfname + ".json";
+
+  ctfcat.children.forEach(channel => {
+    const all_messages_array = lots_of_messages_getter(channel);
+
+    working_obj[channel.name] = {}
     
-    var fs = require('fs');
-    export_obj = JSON.stringify(final_obj); //convert it back to json
-    fs.writeFile('log.json', export_obj, 'utf8', function(err){
-      if(err) throw err;
-    }); // write it back 
+    all_messages_array.then(function(messages) { 
+      for (const msg of messages.reverse()) {
+        working_obj = add_elts_to_obj(working_obj, msg.author.username, msg.content, msg.id, channel.name);
+      }
+      console.log(working_obj);
+
+      var fs = require('fs');
+      export_obj = JSON.stringify(working_obj); //convert it back to json
+
+      /* Eventually you will need this :)
+      * https://discord.com/developers/docs/reference#snowflakes
+      */
+
+      fs.writeFile(`${file_path}`, export_obj, 'utf8', function(err){
+        if(err) throw err;
+      });
+    });
   });
 
-  message.channel.send("Testing message.", { files: ["./log.json"] });
-
+  message.channel.send(`Here's all the data for \`${ctfname}\``, { files: [file_path] });
 };
+
+function add_elts_to_obj(obj, author, content, id, channel_name) {
+  obj[channel_name][id] = {};
+  obj[channel_name][id].author = author;
+  obj[channel_name][id].content = content;
+  return obj;
+}
 
 // borrowed from https://stackoverflow.com/a/55153808
 async function lots_of_messages_getter(channel) {
