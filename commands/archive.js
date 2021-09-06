0cf6232e082ea6c5b0fafd94ec340ca38570885a
Micah Pope <mpope316@gmail.com>
2021-09-05 22:37:15 -0500

    Add capability to handle images as CDN urls

--- commands/archive.js
+++ commands/archive.js
@@ -31,7 +31,13 @@ exports.run = async (client, message, [ctf]) => {
     // const all_messages_array = lots_of_messages_getter(channel);
     all_the_messages.then(function(messages) { 
       for (const msg of messages.reverse()) {
-        working_obj = add_elts_to_obj(working_obj, msg.author.username, msg.content, msg.id, channel.name);
+        var content;
+        try{
+          content = msg.attachments.first().url;
+        } catch {
+          content = msg.content;
+        }
+        working_obj = add_elts_to_obj(working_obj, msg.author.username, content, msg.id, channel.name);
       }
       export_obj = JSON.stringify(working_obj); //convert it back to json
       var fs = require('fs');
